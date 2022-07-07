0dcfa2b66975373ef96352885725739d146a6881
NameIsFill <filpopowski@gmail.com>
2022-07-07 02:56:40 +0200

    working grid and key events

--- src/index.js
+++ src/index.js
@@ -1,5 +1,79 @@
 import './styles.css';
 
-const paragraph = document.createElement('p');
-paragraph.innerText = 'Hello!';
-document.body.append(paragraph);
+const gameGrid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const visibleGameGrid = document.querySelector('#gameGrid');
+// const characterPlayer = document.createElement()
+
+function createGameGrid() {
+  for (let i = 0; i < gameGrid.length; i++) {
+    for (let j = 0; j < gameGrid[i].length; j++) {
+      let cell = document.createElement('div');
+      cell.classList.add('newCell');
+      cell.innerHTML = 'x';
+      visibleGameGrid.appendChild(cell);
+    }
+  }
+}
+
+createGameGrid();
+
+function characterMovement(keyboardKey) {
+  //up
+  if (keyboardKey.keyCode === 38) {
+    alert('up');
+  }
+  //right-up
+  if (keyboardKey.keyCode === 33) {
+    alert('right, up');
+  }
+  //right
+  if (keyboardKey.keyCode === 39) {
+    alert('right');
+  }
+  //right-down
+  if (keyboardKey.keyCode === 34) {
+    alert('right, down');
+  }
+  //down
+  if (keyboardKey.keyCode === 40) {
+    alert('down');
+  }
+  //left-down
+  if (keyboardKey.keyCode === 35) {
+    alert('left, down');
+  }
+  //left
+  if (keyboardKey.keyCode === 37) {
+    alert('left');
+  }
+  //left-up
+  if (keyboardKey.keyCode === 36) {
+    alert('left, up');
+  }
+}
+
+window.addEventListener('keyup', (event) => {
+  characterMovement(event);
+});
